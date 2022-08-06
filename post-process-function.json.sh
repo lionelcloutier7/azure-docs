5f65f3fef9d2b275727f20d9a60cd444bacfdf76
lionelcloutier7 <lionel.cloutier.25@gmail.com>
2022-08-06 10:15:12 +0100

    Add env vars and scripts for app settings
    You need to set some env vars as well. E.g.
    
    export PRODUCT_ITEMS_DOCUMENTDB_CONNECTION_STRING=...
    export TRANSACTIONS_EVENT_HUB_CONNECTION_STRING=...
    export POT_FUNCTION_APP_STORAGE_ACCOUNT_CONNECTION_STRING=...
    
    The values can be copied from the Azure dashboard.

---
+++ post-process-function.json.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+# Script to be run post build to set the content for running the function locally using the environment variable settings
+sed -i -e "s@POT_FUNCTION_APP_STORAGE_ACCOUNT_CONNECTION_STRING@$POT_FUNCTION_APP_STORAGE_ACCOUNT_CONNECTION_STRING@g" target/azure-functions/*/local.settings.json
+sed -i -e "s@TRANSACTIONS_EVENT_HUB_CONNECTION_STRING@$TRANSACTIONS_EVENT_HUB_CONNECTION_STRING@g" target/azure-functions/*/local.settings.json
+sed -i -e "s@PRODUCT_ITEMS_DOCUMENTDB_CONNECTION_STRING@$PRODUCT_ITEMS_DOCUMENTDB_CONNECTION_STRING@g" target/azure-functions/*/local.settings.json
