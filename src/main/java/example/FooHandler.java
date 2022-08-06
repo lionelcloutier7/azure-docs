0196086758f1eb0480182d078b6c75d1fc786eaa
lionelcloutier7 <lionel.cloutier.25@gmail.com>
2022-08-06 05:20:09 +0100

    Switch to latest versions

--- src/main/java/example/FooHandler.java
+++ src/main/java/example/FooHandler.java
@@ -16,15 +16,11 @@
 
 package example;
 
-import java.util.UUID;
-
-import com.microsoft.azure.serverless.functions.ExecutionContext;
-import com.microsoft.azure.serverless.functions.OutputBinding;
-import com.microsoft.azure.serverless.functions.annotation.DocumentDBOutput;
-import com.microsoft.azure.serverless.functions.annotation.EventHubTrigger;
-import com.microsoft.azure.serverless.functions.annotation.FunctionName;
-
-import org.json.JSONObject;
+import com.microsoft.azure.functions.ExecutionContext;
+import com.microsoft.azure.functions.OutputBinding;
+import com.microsoft.azure.functions.annotation.CosmosDBOutput;
+import com.microsoft.azure.functions.annotation.EventHubTrigger;
+import com.microsoft.azure.functions.annotation.FunctionName;
 
 /**
  * @author Soby Chacko
@@ -33,15 +29,13 @@ public class FooHandler {
 
 	@FunctionName("Update-Inventory")
 	public void update(
-			@EventHubTrigger(name = "data", eventHubName = "inventoryeh", connection = "InventoryEventHubTransactionsConnectionString") String data,
-			@DocumentDBOutput(name = "document", databaseName = "inventory", collectionName = "transactions", connection = "InventoryCosmosDBConnectionString", createIfNotExists = true) OutputBinding<String> document,
+			@EventHubTrigger(name = "data", eventHubName = "events", connection = "TRANSACTIONS_EVENT_HUB_CONNECTION_STRING") String data,
+			@CosmosDBOutput(name = "document", databaseName = "inventory", collectionName = "messages", connectionStringSetting = "PRODUCT_ITEMS_DOCUMENTDB_CONNECTION_STRING", createIfNotExists = true) OutputBinding<String> document,
 			final ExecutionContext context) {
 
 		context.getLogger()
 				.info("Java Event Hub transaction trigger processed a request: " + data);
-		JSONObject eventGridMessage = new JSONObject(data);
-		eventGridMessage.put("id", UUID.randomUUID().toString());
-		context.getLogger().info("message: " + eventGridMessage.toString());
-		document.setValue(eventGridMessage.toString());
+		context.getLogger().info("message: " + data);
+		document.setValue(data);
 	}
 }
