22a13103f0e9889df501c9ee3e09d71fc12ce894
lionelcloutier7 <lionel.cloutier.25@gmail.com>
2022-08-07 06:58:51 +0100

    Merge into one class

--- src/main/java/example/FooHandler.java
+++ src/main/java/example/FooHandler.java
@@ -16,18 +16,33 @@
 
 package example;
 
+import java.util.function.Function;
+
 import com.microsoft.azure.functions.ExecutionContext;
 import com.microsoft.azure.functions.OutputBinding;
 import com.microsoft.azure.functions.annotation.CosmosDBOutput;
 import com.microsoft.azure.functions.annotation.EventHubTrigger;
 import com.microsoft.azure.functions.annotation.FunctionName;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.function.adapter.azure.AzureSpringBootRequestHandler;
+import org.springframework.context.annotation.Bean;
 
 /**
  * @author Soby Chacko
  */
+@SpringBootApplication
 public class FooHandler extends AzureSpringBootRequestHandler<Foo, Bar> {
+
+	public static void main(String[] args) throws Exception {
+		SpringApplication.run(FooHandler.class, args);
+	}
+
+	@Bean
+	public Function<Foo, Bar> uppercase() {
+		return foo -> new Bar(foo.getValue().toUpperCase());
+	}
 
 	@FunctionName("Foo-Bar")
 	public void update(
@@ -36,6 +51,55 @@ public class FooHandler extends AzureSpringBootRequestHandler<Foo, Bar> {
 			@CosmosDBOutput(name = "document", databaseName = "inventory", collectionName = "messages", //
 					connectionStringSetting = "PRODUCT_ITEMS_DOCUMENTDB_CONNECTION_STRING", createIfNotExists = true) //
 			OutputBinding<Bar> document, final ExecutionContext context) {
-		document.setValue(handleRequest(data, context));
+		handleOutput(data, document, context);
 	}
+}
+
+class Foo {
+
+	private String value;
+
+	Foo() {
+	}
+
+	public String lowercase() {
+		return value.toLowerCase();
+	}
+
+	public Foo(String value) {
+		this.value = value;
+	}
+
+	public String uppercase() {
+		return value.toUpperCase();
+	}
+
+	public String getValue() {
+		return value;
+	}
+
+	public void setValue(String value) {
+		this.value = value;
+	}
+}
+
+class Bar {
+
+	private String value;
+
+	Bar() {
+	}
+
+	public Bar(String value) {
+		this.value = value;
+	}
+
+	public String getValue() {
+		return value;
+	}
+
+	public void setValue(String value) {
+		this.value = value;
+	}
+
 }
